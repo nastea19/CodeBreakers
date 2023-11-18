7053441b82b47fb7fd458e83ad72acd7dcf61856
nastea19 <nastea.popova1907@gmail.com>
2023-11-18 20:49:56 +0200

    random

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,13 +5,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import APL from "./components/apl";
+import Footer from "./components/footer";
 import NavBar from "./components/navbar";
 import Routing from "./components/Routing";
 import Quiz from "./components/test";
 
 function App() {
   return (
-    <><NavBar /><Routing /></>
+    <><NavBar /><Routing /> <Footer /></>
 
 
   );
