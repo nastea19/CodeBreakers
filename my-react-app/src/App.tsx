d3d6fd80a1fe05525f17564bf1748ea1232ee4c6
Cononciuc Alina <akononciuc@gmail.com>
2023-11-18 21:06:02 +0200

    Alina 2

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,16 +5,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import APL from "./components/apl";
+import Footer from "./components/footer";
 import NavBar from "./components/navbar";
 import Routing from "./components/Routing";
 import Quiz from "./components/test";
-import Routing from "./components/Routing";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routing />
+      <Routing /> <Footer />
     </>
   );
 }
