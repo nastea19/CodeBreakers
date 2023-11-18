c8cb908fdc7ecf78e4d8e61f82ab78233de582cd
nastea19 <nastea.popova1907@gmail.com>
2023-11-18 17:10:46 +0200

    navbar

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import NavigationBar from "./components/NavigationBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import NavBar from "./components/navbar";
 
 function App() {
   return (
-    <h1>MAIN</h1>
+    <><NavBar /></>
+
   );
 }
 
